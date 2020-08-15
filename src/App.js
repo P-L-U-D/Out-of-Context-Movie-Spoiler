dabd2ba19c6894db7e118ccb9abc02917cd1672a
Philip Turkiewicz <philip.turkiewicz@live.ca>
2020-08-15 11:20:16 -0400

    wrong branch woops

--- src/App.js
+++ src/App.js
@@ -4,6 +4,28 @@ import React, { Component } from 'react';
 // import randomizer from './randomizer';
 import './styles/styles.scss'
 
+// Get 3 Api calls
+//   - Api 1 movie search based on user's search we get movie id
+//     - Api 2 based movie id we get a list of keywords
+//       - filter giphy api based of the keywords we get
+// All the apis are going to be one after the other(synchronous)
+// Display data on the page
+// Need two views
+//   - View 1: Search bar will have api #1(moviedatabase)
+//     - View 2: Giphy api
+// Components
+//   - 2 Components
+//     - First component search bar / landing page
+//       - Second component display giphy
+// Error Handling
+//   - error state when call comes back false
+//     - have to make sure that user inputs something in the search bar
+//       - no Numbers!!
+// Stretch Goals
+//   - save and delete options
+//     - accessing my save section and giving user's their own section
+//       - allowing user to choose which gif to delete gif and re - randomize(maybe another api call ?)
+
 class App extends Component {
   render() {
     return (
--- src/SearchBar.js
+++ src/SearchBar.js
@@ -1,8 +1,27 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 
 
 class SearchBar extends Component {
+    constructor() {
+        super();
+        this.state = {
+            placeholder: []
+        }
+    }
+
+    componentDidMount() {
+    // API CALL 1: movie search based on user's search 
+        // return the MovieID (also have access to movie details)
+    // API CALL 2: based on MovieID from call 1 
+        // return array of keywords
+    // wrap calls in async fucnction? use await to get response from API 1 before we call API 2
+    // on successful return of API 2, 
+
+    console.log('did mount.')
+    }
+
     render() {
         return (
             <></>
