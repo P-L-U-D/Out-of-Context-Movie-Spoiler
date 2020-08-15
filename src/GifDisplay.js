c439ecca406e19aa1fdc5c5697352ed40e6ef0a9
DanielKonkine <dkonkine@yahoo.ca>
2020-08-14 20:40:19 -0400

    first commit

---
+++ src/GifDisplay.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class GifDisplay extends Component {
+    render() {
+        return (
+            <></>
+        )
+    }
+}
+
+
+
+export default GifDisplay;
