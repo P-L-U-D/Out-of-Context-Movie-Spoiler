7d99d86b215f35ee8d0253f89e0118a513d6fdbd
Philip Turkiewicz <philip.turkiewicz@live.ca>
2020-08-15 11:36:10 -0400

    spread out pseudocode and add some more base structure

--- src/SearchBar.js
+++ src/SearchBar.js
@@ -16,13 +16,20 @@ class SearchBar extends Component {
         // return the MovieID (also have access to movie details)
     // API CALL 2: based on MovieID from call 1 
         // return array of keywords
+        // randomly pick 3 words and save
     // wrap calls in async fucnction? use await to get response from API 1 before we call API 2
-    // on successful return of API 2, 
+    // on successful return of API 2, pass saved keywords to GifDisplay component & trigger view switch
+
+    // ERROR CATCH: when user types in empty string, don't submit the call & prompt user to write a word
+    // ERROR CATCH: if user types in a string that is not a direct match, return closest possible match
+        // STRETCH GOAL: instead of closest match, return a list of possible matches and allow the user to pick the one they want
+    // ERROT CATCH: NO NUMBERS! people work the regex magic
 
     console.log('did mount.')
     }
 
     render() {
+        // Just a search bar (text input)
         return (
             <></>
         )
