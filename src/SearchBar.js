29bc2badb0a13f950824643ae51ba24e0aba9175
DanielKonkine <dkonkine@yahoo.ca>
2020-08-16 15:56:13 -0400

    Added onChange handler, and added popularity filter based on user input

--- src/SearchBar.js
+++ src/SearchBar.js
@@ -2,71 +2,107 @@ import React, { Component } from 'react';
 import axios from 'axios';
 
 
+        
+//         // randomly pick 3 words and save
+//         // wrap calls in async fucnction? use await to get response from API 1 before we call API 2
+//         // on successful return of API 2, pass saved keywords to GifDisplay component & trigger view switch
+
+//         // ERROR CATCH: when user types in empty string, don't submit the call & prompt user to write a word
+//         // ERROR CATCH: if user types in a string that is not a direct match, return closest possible match
+//             // STRETCH GOAL: instead of closest match, return a list of possible matches and allow the user to pick the one they want
+//         // ERROT CATCH: NO NUMBERS! people work the regex magic
+
+
+
 
 class SearchBar extends Component {
     constructor() {
         super();
         this.state = {
             movieSearch: [],
-            keywordSearch: []
+            movieID: [],
+            keywordSearch: [],
+            moviedbAPI: 'b588f737df1d6878d6133a1a7e0bface',
+            giphyAPI: 'NShPdQTfWnvbvgxLo7Jd7C5qDeFfrsLR',
+            userInput: ""
         }
     }
-
-    componentDidMount() {
-    // API CALL 1: movie search based on user's search 
+    getMovie = (event) => {
+        event.preventDefault();
+        // API CALL 1: movie search based on user's search 
         // return the MovieID (also have access to movie details)
-    
         axios({
             url: 'https://api.themoviedb.org/3/search/movie?',
             params: {
-                api_key: 'b588f737df1d6878d6133a1a7e0bface',
-                query: 'The+Dirt',
+                method: `GET`,
+                responseType: `json`,
+                api_key: this.state.moviedbAPI,
+                query: `${this.state.userInput}`,
+                include_adult: 'false',
                 page: 1
             }
         })
         .then((res) => {
-            const movieRes = res.data.results;
-            
-            this.setState({
-                movieSearch: movieRes
-            })
-            console.log(movieRes);
-        })
-    // API CALL 2: based on MovieID from call 1 
-        // return array of keywords
+            // const movieRes = res.data.results;
+            let popularity = 0
 
-        axios({
-            url: `https://api.themoviedb.org/3/movie/75780/keywords?`,
-            params: {
-                api_key: 'b588f737df1d6878d6133a1a7e0bface',
-            }
-        })
-        .then((res) => {
-            const keywordID = res.data.keywords
+            let movieObject;
+
+            res.data.results.forEach( (i) => {
+                if(i.popularity > popularity) {
+                    movieObject = i
+                    popularity = i.popularity
+                }
+            })
+
 
             this.setState({
-                keywordSearch: keywordID
+                movieSearch: movieObject
             })
-            console.log(keywordID);
+            console.log(movieObject);
+        }).catch(error => {
+            console.log('something went wrong');
         })
-        // randomly pick 3 words and save
-    // wrap calls in async fucnction? use await to get response from API 1 before we call API 2
-    // on successful return of API 2, pass saved keywords to GifDisplay component & trigger view switch
+    }
 
-    // ERROR CATCH: when user types in empty string, don't submit the call & prompt user to write a word
-    // ERROR CATCH: if user types in a string that is not a direct match, return closest possible match
-        // STRETCH GOAL: instead of closest match, return a list of possible matches and allow the user to pick the one they want
-    // ERROT CATCH: NO NUMBERS! people work the regex magic
+
+    handleUserInput = (event) => {
+        this.setState({
+            userInput: event.target.value
+        })
     }
 
     render() {
         // Just a search bar (text input)
         return (
-            <></>
+            <form onSubmit={this.getMovie} action="">
+                <label htmlFor=""></label>
+                <input onChange={this.handleUserInput} type="text"
+                    placeholder="e.g. Fight Club"
+                    id="" required />
+                <button type="submit">Search</button>
+            </form>
         )
     }
 }
 
-
-
 export default SearchBar;
+
+
+
+
+//API call 2, return keywords based on query search from API call 1
+// axios({
+//     url: `https://api.themoviedb.org/3/movie/75780/keywords?`,
+//     params: {
+//         api_key: 'b588f737df1d6878d6133a1a7e0bface',
+//     }
+// })
+// .then((res) => {
+//     const keywordID = res.data.keywords
+
+//     this.setState({
+//         keywordSearch: keywordID
+//     })
+//     console.log(keywordID);
+// })
